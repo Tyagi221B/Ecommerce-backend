47637b50d40662c0369169d56a160ff3d6cd2b11
Asmit tyagi <ashmittyagigms@gmail.com>
2024-06-11 18:31:46 +0530

    write models, controllers and routes for products, orders, payments, and coupons etc. also add middelwares. write controller for stats.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,54 @@
-import express from 'express';
-import { connectDB } from './utils/features.js';
-import userRoute from './routes/user.js';
+import express from "express";
+import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import NodeCache from "node-cache";
+import { config } from "dotenv";
+import morgan from "morgan";
+import Stripe from "stripe";
+import cors from "cors";
 
+// Importing Routes
+import userRoute from "./routes/user.js";
+import productRoute from "./routes/products.js";
+import orderRoute from "./routes/order.js";
+// import paymentRoute from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
 
-const port = 4000;
+config({
+  path: "./.env",
+});
+
+console.log(process.env.PORT);
+
+const port = process.env.PORT || 8000;
+const mongoURI = process.env.MONGODB_URI || "";
+const stripeKey = process.env.STRIPE_KEY || "";
+
+connectDB(mongoURI);
+
+export const stripe = new Stripe(stripeKey);
+export const myCache = new NodeCache();
 
 const app = express();
-connectDB("mongodb+srv://ashmittyagigms:Uh5CzAjOqPo4zXBN@cluster0.avlbwzc.mongodb.net/");
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API Working with /api/v1");
 });
+
+// Using Routes
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/product", productRoute);
+app.use("/api/v1/order", orderRoute);
+// app.use("/api/v1/payment", paymentRoute);
+app.use("/api/v1/dashboard", dashboardRoute);
+
+app.use("/uploads", express.static("uploads"));
+app.use(errorMiddleware);
 
 app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
+  console.log(`Express is working on http://localhost:${port}`);
 });
