61f6cc18985e6788564f6f1505a2988a71d83d5d
Asmit tyagi <ashmittyagigms@gmail.com>
2024-09-20 23:35:11 +0530

    .

---
+++ src/models/DiamondQualityOptions.ts
@@ -0,0 +1,19 @@
+import { Schema, model, Document, HydratedDocument } from 'mongoose';
+
+export interface IDiamondQualityOption extends Document {
+  product: Schema.Types.ObjectId;
+  qualityGrade: string;
+  diamondPriceMultiplier: number;
+}
+
+export type IDiamondQualityOptionDocument = HydratedDocument<IDiamondQualityOption>;
+
+
+const DiamondQualityOptionSchema = new Schema<IDiamondQualityOption>({
+  product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+  qualityGrade: { type: String, required: true },
+  diamondPriceMultiplier: { type: Number, required: true },
+});
+
+export const DiamondQualityOption = model<IDiamondQualityOption>('DiamondQualityOption', DiamondQualityOptionSchema);
+
